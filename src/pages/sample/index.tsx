e0c8b336c87f75787745412386f5cd0d344472da
Nke0628 <ooo_diva@yahoo.co.jp>
2024-02-19 22:33:53 +0900

    refactor: storybook用に一時的にサンプルページをコメントアウト

--- src/pages/sample/index.tsx
+++ src/pages/sample/index.tsx
@@ -4,25 +4,25 @@ import { ReactElement } from "react";
 import { useQuery } from "urql";
 
 export default function Page() {
-  const queryTest = `
-    query login {
-      getjwt {
-        jwt
-      }
-    }
-  `;
-  const [result] = useQuery({
-    query: queryTest,
-  });
+  // const queryTest = `
+  //   query login {
+  //     getjwt {
+  //       jwt
+  //     }
+  //   }
+  // `;
+  // const [result] = useQuery({
+  //   query: queryTest,
+  // });
 
   return (
     <div>
       <ChakuraButton>chakura button</ChakuraButton>
-      {result.data && alert(result.data.jwt)}
+      {/* {result.data && alert(result.data.jwt)} */}
     </div>
   );
 }
 
-Page.getLayout = function getLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>;
-};
+// Page.getLayout = function getLayout(page: ReactElement) {
+//   return <Layout>{page}</Layout>;
+// };
